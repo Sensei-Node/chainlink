618c7168b90c64b26e935e8db5ca3a76d004cb4b
Ubuntu <ubuntu@senseinode-chainlink-poctestnet.ce2dks1bglvuzkyshvxrhimqhh.bx.internal.cloudapp.net>
2023-01-19 17:51:16 +0000

    Add eth price script cron

--- chainlink_scheduler/scheduler_request_dolar.sh
+++ chainlink_scheduler/scheduler_request_dolar.sh
@@ -5,9 +5,9 @@ HERE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 set -a
 source ${HERE}/.env
 
-echo "[ $(date;) ] - BEGIN" >> $LOG_FILE
-echo "[ $(date;) - NPX VERSION:  $(npx --version)" >> $LOG_FILE
+echo "[ $(date;) ] - BEGIN" >> $LOG_FILE_DOLAR
+echo "[ $(date;) - NPX VERSION:  $(npx --version)" >> $LOG_FILE_DOLAR
 
 cd ${HERE}
-npx hardhat requestDolar --verbose --client-address "${CL_CLIENT_ADDRESS}" --operator-address "${CL_OPERATOR_ADDRESS}" --job-id "${CL_JOB_ID}" >>${LOG_FILE}
-echo "[ $(date;) ] - END" >> $LOG_FILE
+npx hardhat requestDolar --verbose --client-address "${CL_CLIENT_ADDRESS_DOLAR}" --operator-address "${CL_OPERATOR_ADDRESS_DOLAR}" --job-id "${CL_JOB_ID_DOLAR}" >>${LOG_FILE_DOLAR}
+echo "[ $(date;) ] - END" >> $LOG_FILE_DOLAR
--- chainlink_scheduler/scheduler_request_eth.sh
+++ chainlink_scheduler/scheduler_request_eth.sh
@@ -5,9 +5,9 @@ HERE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 set -a
 source ${HERE}/.env
 
-echo "[ $(date;) ] - BEGIN" >> $LOG_FILE
-echo "[ $(date;) - NPX VERSION:  $(npx --version)" >> $LOG_FILE
+echo "[ $(date;) ] - BEGIN" >> $LOG_FILE_ETH
+echo "[ $(date;) - NPX VERSION:  $(npx --version)" >> $LOG_FILE_ETH
 
 cd ${HERE}
-npx hardhat requestEth --verbose --client-address "${CL_CLIENT_ADDRESS}" --operator-address "${CL_OPERATOR_ADDRESS}" --job-id "${CL_JOB_ID}" >>${LOG_FILE}
-echo "[ $(date;) ] - END" >> $LOG_FILE
+npx hardhat requestEth --verbose --client-address "${CL_CLIENT_ADDRESS_ETH}" --operator-address "${CL_OPERATOR_ADDRESS_ETH}" --job-id "${CL_JOB_ID_ETH}" >>${LOG_FILE_ETH}
+echo "[ $(date;) ] - END" >> $LOG_FILE_ETH
